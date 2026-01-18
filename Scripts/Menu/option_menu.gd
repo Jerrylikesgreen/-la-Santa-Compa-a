3c636849864fbeebbd0c8c0a12748edbb3d28533
PyramidHead01 <73906531+PyramidHead01@users.noreply.github.com>
2026-01-18 14:31:52 +0100

    terminado menu de inicio

--- Scripts/Menu/option_menu.gd
+++ Scripts/Menu/option_menu.gd
@@ -1,1 +1,11 @@
 extends CanvasLayer
+
+
+
+@onready var titulo_menu = get_tree().get_nodes_in_group("titulo_menu")[0]
+@onready var menu_base = get_tree().get_nodes_in_group("menu_base")[0]
+
+func _on_button_pressed() -> void:
+	hide()
+	titulo_menu.show()
+	menu_base.show()
--- Scripts/Menu/start_menu.gd
+++ Scripts/Menu/start_menu.gd
@@ -2,6 +2,11 @@ extends CanvasLayer
 
 @export var first_scene = "res://Scenes/black_scene.tscn"
 
+@onready var option_menu = $Control/OptionMenu
+
+@onready var titulo = $Control/Titulo
+@onready var inicio = $Control/Inicio
+
 func _on_bot_continue_pressed() -> void:
 	get_tree().change_scene_to_file(first_scene)
 
@@ -11,7 +16,15 @@ func _on_bot_new_game_pressed() -> void:
 
 
 func _on_bot_options_pressed() -> void:
-	pass # Replace with function body.
+	#if option_menu.is_visible():
+		#option_menu.hide()
+	#else: option_menu.show()
+
+	titulo.hide()
+	inicio.hide()
+
+	option_menu.show()
+	
 
 
 func _on_bot_exit_pressed() -> void:
